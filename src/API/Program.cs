801c8dc9da8bf28899315d9b17a88c3625ca1910
markld <thakr3@gmail.com>
2026-02-11 01:34:51 +0700

    add swagger

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,23 +1,68 @@
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddCors(options =>
+    options.AddDefaultPolicy(policy =>
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod()));
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Project Lifecycle Service API",
+        Version = "v1",
+    });
+
+    //c.CustomSchemaIds(type => (type.FullName ?? type.Name ?? type.ToString()).Replace("+", "."));
+});
+
+builder.Services.AddProblemDetails();
+
+//builder.Services.AddApplication();
+//builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddHealthChecks();
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+
+app.UseSwaggerUI(options =>
 {
-    app.MapOpenApi();
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
+app.UseCors();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/error-development");
+}
+else
+{
+    app.UseExceptionHandler("/error");
+}
 
 app.UseAuthorization();
-
 app.MapControllers();
+
+// Map Minimal API endpoints
+//app.MapHealthcareEndpoints();
 
 app.Run();
